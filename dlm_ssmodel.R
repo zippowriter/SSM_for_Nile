6bb6c0a36f04d82aa4675dde5ddb8c86f918413d
haruhi <haruhi.kono@me.com>
2020-12-15 19:00:53 +0900

    modified dlm_ssmodel

--- dlm_ssmodel.R
+++ dlm_ssmodel.R
@@ -1,24 +1,3 @@
-# dlmのパラメタ設定
-mod_dlm <- dlmModPoly(
-  order = 1, m0 = 0, C0 = 10000000, dW = sigma_w, dV = sigma_v
-)
-
-mu_filter_dlm <- dlmFilter(Nile, mod_dlm)
-
-mu_filter_dlm$m
-mu_filter
-sum((mu_filter_dlm$m[-1] - mu_filter[-1])^2)
-
-# 対数尤度の指標
-dlmLL(Nile, mod_dlm)
-# 比較
-1/2 * sum(log(F) + y_resid^2 / F)
-
-mu_smooth_dlm <- dlmSmooth(mu_filter_dlm)
-mu_smooth_dlm$s
-mu_smooth
-sum((mu_smooth_dlm$s - mu_smooth)^2)
-
 # Step1 モデルの構造を決める
 build_local_level_dlm <- function(theta){
   dlmModPoly(order = 1, dV = exp(theta[1]), dW = exp(theta[2]))
